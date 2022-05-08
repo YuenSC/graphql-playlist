18c5e2c4edea99a141f3f3f13a4deca9143b62ca
Calvin Yuen <scyuenab@gmail.com>
2022-05-08 15:43:37 +0800

    project init

---
+++ README.md
@@ -0,0 +1,13 @@
+This is a code-along of a grpahql tutorial from https://www.youtube.com/playlist?list=PL4cUxeGkcC9iK6Qhn-QLcXCXPQUov1U7f
+
+This reamde try to record any useful thinig by the lecture number in the above youtube playlist.
+
+## L6
+
+1. use express-graphql to enable graphql in express
+2. need to build a schema before using
+
+## L7 (Build schema)
+
+1. define object type
+2. fields in object type should be a function
---
+++ server/app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+var { buildSchema } = require("graphql");
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return "Hello world!";
+  },
+};
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () => {
+  console.log("now listening on port 4000");
+});
